332a1a446b63e3e77fd4ea6ba215bda8c283afbe
민Hieu <hieu1052k3@gmail.com>
2020-02-16 11:12:00 +0700

    .
    .

--- Supercharge Automation/Python Code/Super_Test.py
+++ Supercharge Automation/Python Code/Super_Test.py
@@ -2,6 +2,7 @@
 from Super_Func import *
 from Super_Seq import *
 
+
 def console():
     # seq1 = input("1st protein:  ")
     # name1 = input("1st protein name:  ")
@@ -13,14 +14,13 @@ def console():
     HTDGIPHLVL SPSTVFFGAF AL"""
 
     threshold = 1
-    
+
     protein = Super_Seq(seq_test, name="TNFB")
     binding_site = binding_site_converter("20-23+27")
-    charged_protein = supercharge_arbitrary(protein, threshold=threshold, charge=1, binding_site=binding_site)
+    charged_protein = supercharge_arbitrary(
+        protein, threshold=threshold, charge=1, binding_site=binding_site)
 
     protein.compare_lcd(charged_protein, threshold=threshold)
 
 
 console()
-
-
